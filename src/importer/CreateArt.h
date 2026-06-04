3620b7257ce28ce4643537b5397d6ee4336163cc
sillerasd <sillerasd@gmail.com>
2026-06-04 04:19:41 +0300

    Update CreateArt.h

--- src/importer/CreateArt.h
+++ src/importer/CreateArt.h
@@ -20,83 +20,67 @@ static auto ALLOWED_TYPES2 = file::FilePickOptions{
 
 class CreateArt final{
 private:
-    // bunch if stuff I need to know
-    // object IDs for various object sizes
     static constexpr int pixelObjID = 3097;
     static constexpr int medPixelObjID = 3094;
     static constexpr int bigPixelObjID = 3093;
     static constexpr int largePixelObjID = 3092;
-    // object ID of old pixel object
     static constexpr int oldPixelObjID = 917;
-    // large old pixel
-	static constexpr int oldLargePixelObjID = 211;
-    // z order layering
+    static constexpr int oldLargePixelObjID = 211;
     static constexpr int zOrder = 1;
-    // size of the objects
     static constexpr float objSize = 5.0f;
-    // scale used for moving between pixels
     static constexpr float scale = 5;
 
-    // number of channels - usually 3 for jpg and 4 for png
     int channels = 4;
-    // sixes
     int height = 0;
     int width = 0;
 
-	uint8_t Mr = 0;
-	uint8_t Mg = 0;
-	uint8_t Mb = 0;
+    float scaleMulX = 1.0f;   // ← ДОБАВЛЕНО
+    float scaleMulY = 1.0f;   // ← ДОБАВЛЕНО
 
-    // image data
+    uint8_t Mr = 0;
+    uint8_t Mg = 0;
+    uint8_t Mb = 0;
+
     unsigned char* data = nullptr;
 
-    // object string
     std::ostringstream objInLevel;
 
-    // selected object
     GameObject* obj = nullptr;
 
-    // closing the menu after complete
     std::function<void()> closeMenu = nullptr;
 
-    // settings
     bool limitSize = Mod::get()->getSettingValue<bool>("Disable-limit");
     int sizeLimit = Mod::get()->getSettingValue<int>("Size-limit");
     int colourChannel = Mod::get()->getSettingValue<int>("Colour-channel");
-	bool useOldPixel = Mod::get()->getSettingValue<bool>("Use-OlderObjects");
-	int tolerance = Mod::get()->getSettingValue<int>("Tolerance");
-	std::string basic = Mod::get()->getSettingValue<std::string>("Optimise-Type");
-	bool backgroundOp = Mod::get()->getSettingValue<bool>("Background-Optimisation");
+    bool useOldPixel = Mod::get()->getSettingValue<bool>("Use-OlderObjects");
+    int tolerance = Mod::get()->getSettingValue<int>("Tolerance");
+    std::string basic = Mod::get()->getSettingValue<std::string>("Optimise-Type");
+    bool backgroundOp = Mod::get()->getSettingValue<bool>("Background-Optimisation");
 
-    // places the art with the path
     void placeArt(const std::string& p);
 
-    // helper methods - methods of importing the art
     void simpleImport(const std::string& p);
-	void basicOptimiseImport(const std::string& p);
-	void scaleOptimiseImport(const std::string& p);
+    void basicOptimiseImport(const std::string& p);
+    void scaleOptimiseImport(const std::string& p);
     void optimiseJPG();
 
-    // other helpers
     void formatHSV(float red, float green, float blue, std::string& objColour) const;
     void RGBtoHSV(float& r, float& g, float& b) const;
     int bestFit(std::vector<std::vector<bool>>& p, unsigned char const* data, int x, int y, int ch, int wid, int hi);
     bool comparePixels(unsigned char const*& data, int p1, int p2) const;
     bool comparePixels(uint8_t r1, uint8_t g1, uint8_t b1, uint8_t r2, uint8_t g2, uint8_t b2) const;
     int scalePixX(std::vector<std::vector<bool>>& p, unsigned char const* data, int x, int y, int ch, int wid, int hi);
-	int scalePixY(std::vector<std::vector<bool>>& p, unsigned char const* data, int x, int y, int ch, int wid, int hi, int xScale);
+    int scalePixY(std::vector<std::vector<bool>>& p, unsigned char const* data, int x, int y, int ch, int wid, int hi, int xScale);
 
 public:
     CreateArt() {}
     ~CreateArt() {}
 
-    // importing the art
     void importArt();
-
-    // changes the settings before the art is imported
     void updateSettings();
 
-    // setters
     void setSelectedObject(GameObject* o) { obj = o; }
     void setCloseMenu(std::function<void()> c) { closeMenu = c; }
+
+    void setTargetSize(int w, int h);   // ← ДОБАВЛЕНО
 };
